0ffcc47e34122b7296c7d2fabdc9f932984820c9
Bruno Marinho <brunohenrique94@outlook.com>
2024-08-01 15:17:08 -0300

    feat: add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import express, { Request, Response } from 'express';
+import './db/conn';
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send('Teste');
+});
+
+app.listen(PORT, () => {
+  console.log(`Running server on port: ${PORT}`);
+});
